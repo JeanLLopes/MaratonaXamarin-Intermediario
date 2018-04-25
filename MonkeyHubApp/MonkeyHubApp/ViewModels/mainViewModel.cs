2bd07eafdcf705999ed8b62c19725063fcb6def7
jlopes <jean.llopes@outlook.com>
2018-04-24 23:50:45 -0300

    INotifyPropertyChanged

---
+++ MonkeyHubApp/MonkeyHubApp/ViewModels/mainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace MonkeyHubApp.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public string Descricao { get; set; }
+
+        public MainViewModel()
+        {
+            Descricao = "Olá Viewmodel";
+
+            //PARA NOTIFICAR A VIEW DA ATUALIZAÇÃO
+            //DEVEMOS USAR UMA INTERFACE "INotifyPropertyChanged"
+            Task.Delay(3000).ContinueWith(x =>
+            {
+                Descricao = "Meu Texto Mudou";
+
+
+                if (PropertyChanged != null)
+                {
+                    //AQUI NOS ATUALIZAMOS A VIEW USANDO 
+                    //A INTERFACE "INotifyPropertyChanged"
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Descricao"));
+
+                }
+            });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
