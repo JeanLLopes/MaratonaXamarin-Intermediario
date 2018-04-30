14e66da9a17a6a5a97f6ad4b80b8e6cc4cd35622
jlopes <jean.llopes@outlook.com>
2018-04-29 21:00:05 -0300

    Add

--- MonkeyHubApp/MonkeyHubApp/MainPage.xaml.cs
+++ MonkeyHubApp/MonkeyHubApp/MainPage.xaml.cs
@@ -1,26 +1,42 @@
-﻿using MonkeyHubApp.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MonkeyHubApp.Services;
+using MonkeyHubApp.ViewModels;
 using Xamarin.Forms;
 
 namespace MonkeyHubApp
 {
 	public partial class MainPage : ContentPage
 	{
+        private MainPageViewModel ViewModel => BindingContext as MainPageViewModel;
+
 		public MainPage()
 		{
 			InitializeComponent();
+            BindingContext = new MainPageViewModel(new MonkeyHubApiService());
+
+            //CLIQUE DO BOTAO DE ITEM SELECIONADO
+            this.ListViewJornalistas.ItemSelected += (sender, e) =>
+             {
+                 ViewModel.ShowPostCommand.Execute(e.SelectedItem);
+             };
+
 
             //AQUI NOS LIGAMOS A VIEWMODEL A TELA
-            BindingContext = new MainPageViewModel();
+            //BindingContext = new MainPageViewModel();
 
-            BtnNavegar.Clicked += async (sender, e) =>
-            {
-                await Navigation.PushAsync(new SearchPage());
-            };
-		}
-	}
+            //PARA CLIARMOS O CLICK DE UM BOTAO COM NAVEGAÇÃO
+            //BtnNavegar.Clicked += async (sender, e) =>
+            //{
+            //    await Navigation.PushAsync(new SearchPage());
+            //};
+        }
+
+        //REESCREVEMSO O METODO DE LOAD DA PAGINA 
+        protected override void OnAppearing()
+        {
+            //CHAMA O METODO "LoadAsync" PARA FAZER O CARREGAMENTO DA TELA
+            (this.BindingContext as MainPageViewModel)?.LoadAsync();
+            base.OnAppearing();
+        }
+
+    }
 }
